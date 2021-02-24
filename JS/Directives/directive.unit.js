2e5b68f29d33f2cb389cad88dfce2ef2b7c76bf5
IronPegasus <kayleebanana@gmail.com>
2021-02-23 21:46:02 -0600

    21w08a, status condition effects, tag image rotation

--- JS/Directives/directive.unit.js
+++ JS/Directives/directive.unit.js
@@ -1,4 +1,4 @@
-app.directive('unit', function() {
+app.directive('unit', ['$interval', function($interval) {
     return{
         restrict: 'E', //element
         templateUrl: 'HTML/Directives/unit.html',
@@ -6,6 +6,25 @@ app.directive('unit', function() {
             unit: "=unit",
             tileconsts: "=tileconsts",
             tagsysdata: "=tagsysdata"
+        },
+        link: function($scope, element, attrs) {
+            $scope.tagIterator = 0;
+            $scope.filteredTagList = [];
+
+            for(var index in $scope.unit.tags){
+                var tag = $scope.unit.tags[index];
+                if($scope.tagsysdata[tag].showOnUnit)
+                    $scope.filteredTagList.push(tag);
+            }
+            
+            function iterateTag(){
+                if($scope.tagIterator == $scope.filteredTagList.length-1) $scope.tagIterator = 0;
+                else $scope.tagIterator += 1;
+            };
+
+            //Only set up interval event if we have more than one tag
+            if($scope.filteredTagList.length > 1)
+                $interval(iterateTag, 2000);
         }
     };
- });
+ }]);
