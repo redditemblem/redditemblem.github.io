0d81119d907e414e26745b049c90943a4de0e1c9
IronPegasus <kayleebanana@gmail.com>
2021-03-09 19:46:13 -0600

    21w10a, added rotating status condition sprites

--- JS/Directives/directive.unit.js
+++ JS/Directives/directive.unit.js
@@ -5,26 +5,49 @@ app.directive('unit', ['$interval', function($interval) {
         scope: {
             unit: "=unit",
             tileconsts: "=tileconsts",
+            statussysdata: "=statussysdata",
             tagsysdata: "=tagsysdata"
         },
         link: function($scope, element, attrs) {
+            $scope.statusIterator = 0;
             $scope.tagIterator = 0;
-            $scope.filteredTagList = [];
+            
+            $scope.statusSpriteList = [];
+            $scope.tagSpriteList = [];
 
+            //Build status list
+            var addGenericStatusSprite = false;
+            for(var index in $scope.unit.statusConditions){
+                var status = $scope.unit.statusConditions[index];
+                var statusData = $scope.statussysdata[status.name];
+                if(statusData.spriteURL.length > 0)
+                    $scope.statusSpriteList.push(statusData.spriteURL);
+                else addGenericStatusSprite = true;
+            }
+
+            if(addGenericStatusSprite)
+                $scope.statusSpriteList.push("IMG/status_heart.png");
+
+            //Build tag list
             for(var index in $scope.unit.tags){
                 var tag = $scope.unit.tags[index];
-                if($scope.tagsysdata[tag].showOnUnit)
-                    $scope.filteredTagList.push(tag);
+                var tagData = $scope.tagsysdata[tag];
+                if(tagData.showOnUnit && tagData.spriteURL.length > 0)
+                    $scope.tagSpriteList.push(tagData.spriteURL);
             }
             
-            function iterateTag(){
-                if($scope.tagIterator == $scope.filteredTagList.length-1) $scope.tagIterator = 0;
+            function iterateSprites(){
+                if($scope.statusIterator == $scope.statusSpriteList.length-1) $scope.statusIterator = 0;
+                else $scope.statusIterator += 1;
+
+                if($scope.tagIterator == $scope.tagSpriteList.length-1) $scope.tagIterator = 0;
                 else $scope.tagIterator += 1;
             };
 
-            //Only set up interval event if we have more than one tag
-            if($scope.filteredTagList.length > 1)
-                $interval(iterateTag, 2000);
+            //Only set up interval event if we have a list with more than one sprite
+            if(   $scope.statusSpriteList.length > 1
+               || $scope.tagSpriteList.length > 1)
+                $interval(iterateSprites, 2000);
         }
     };
  }]);
