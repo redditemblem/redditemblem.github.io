48fba0a45048bda1947e77e494388d21afd5ed77
Iron Pegasus <kayleebanana@gmail.com>
2024-11-19 20:18:01 -0600

    24w47a, add check for dark mode preference when setting theme

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 var app = angular.module('RedditEmblemViewer', ['ngRoute', 'ngCookies', 'ngMaterial', 'ngAnimate']);
 app.config(['$routeProvider', '$mdThemingProvider', function ($routeProvider, $mdThemingProvider) {
-	$routeProvider
-		.when("/", {templateUrl: "HTML/homepage.html", controller: "HomepageCtrl"})
-		.when("/info", {templateUrl: "HTML/info.html", controller: "InfoCtrl"})
-		.when("/:teamName/map", {templateUrl: "HTML/map.html", controller: "MapCtrl"})
-		.when("/:teamName/map/analyze", {templateUrl: "HTML/analyze.html", controller: "AnalyzeCtrl"})
-		.when('/:teamName/convoy', {templateUrl: "HTML/convoy.html", controller: "ConvoyCtrl"})
-		.when('/:teamName/shop', {templateUrl: "HTML/shop.html", controller: "ShopCtrl"})
-		.otherwise({ redirectTo: "/" });
+    $routeProvider
+        .when("/", {templateUrl: "HTML/homepage.html", controller: "HomepageCtrl"})
+        .when("/info", {templateUrl: "HTML/info.html", controller: "InfoCtrl"})
+        .when("/:teamName/map", {templateUrl: "HTML/map.html", controller: "MapCtrl"})
+        .when("/:teamName/map/analyze", {templateUrl: "HTML/analyze.html", controller: "AnalyzeCtrl"})
+        .when('/:teamName/convoy', {templateUrl: "HTML/convoy.html", controller: "ConvoyCtrl"})
+        .when('/:teamName/shop', {templateUrl: "HTML/shop.html", controller: "ShopCtrl"})
+        .otherwise({ redirectTo: "/" });
 
-	//Force injection of $cookies service
-	var $cookies;
-	angular.injector(['ngCookies']).invoke(['$cookies', function(_$cookies_) {
-		$cookies = _$cookies_;
-	}]);
+    //Force injection of $cookies service
+    var $cookies;
+    angular.injector(['ngCookies']).invoke(['$cookies', function(_$cookies_) {
+        $cookies = _$cookies_;
+    }]);
 
-	//Define color themes
-	$mdThemingProvider.theme('default')
-					  .primaryPalette('blue')
-					  .accentPalette('amber');
-	$mdThemingProvider.theme('dark')
-					  .primaryPalette('blue')
-					  .accentPalette('amber')
-					  .dark();
+    //Define color themes
+    $mdThemingProvider.theme('default')
+                        .primaryPalette('blue')
+                        .accentPalette('amber');
+    $mdThemingProvider.theme('dark')
+                        .primaryPalette('blue')
+                        .accentPalette('amber')
+                        .dark();
 
-	var theme = $cookies.get('RedditEmblemMaps-SavedTheme')
-	if(theme != undefined)
-		$mdThemingProvider.setDefaultTheme(theme);
+    var darkModePreferred = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    var theme = $cookies.get('RedditEmblemMaps-SavedTheme');
+    if(darkModePreferred)
+	$mdThemingProvider.setDefaultTheme('dark');
+    else if(theme != undefined)
+        $mdThemingProvider.setDefaultTheme(theme);
 }]);
